aea3f19032f58c9a4a33055d935711a90c4c7c4c
s-sawa <s.s.hrmeee21@gmail.com>
2023-12-11 23:52:52 +0900

    型の互換性

---
+++ src/type-compatibillity.ts
@@ -0,0 +1,39 @@
+export {};
+
+let fooCompatible: any;
+let barCompatible: string = "TypeScript";
+
+console.log(typeof fooCompatible);
+
+fooCompatible = barCompatible;
+// anyからstring型に変わる
+console.log(typeof fooCompatible);
+
+let fooIncompatible: string;
+let barIncompaatible: number = 1;
+
+// fooIncompatible = barIncompaatible
+
+let fooString: string;
+let barString: string = "string";
+
+fooString = barString;
+
+let fooStringLiteral: "fooStringLiteral" = "fooStringLiteral";
+fooString = fooStringLiteral;
+
+let fooNumber: number;
+let fooNumberLiteral: 1976 = 1976;
+fooNumber = fooNumberLiteral;
+
+interface Animal {
+  age: number;
+  name:string;
+}
+
+class Person {
+  constructor(public age: number, public name:string) {}
+}
+
+let me: Animal;
+me = new Person(43, "shogo");
